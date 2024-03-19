c3c0a7f0b01ce5f40d2b506e1b17dd8bab57c874
anjali-8001 <anjalisharma8001@gmail.com>
2024-03-19 21:40:01 +0530

    first commit

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -26,23 +26,23 @@ function Dashboard() {
   }, [result]);
   return (
     <div>
-      <div class="relative overflow-x-auto ">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto ">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Username
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Language
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Stdin
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Time Stamps
               </th>
-              <th scope="col" class="px-0 py-3">
+              <th scope="col" className="px-0 py-3">
                 Code
               </th>
             </tr>
@@ -51,17 +51,17 @@ function Dashboard() {
             {result && result.length > 0 ? (
               result.map((user) => {
                 return (
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th
                       scope="row"
-                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
                       {user.username}
                     </th>
-                    <td class="px-6 py-4">{user.language}</td>
-                    <td class="px-6 py-4">{user.stdin}</td>
-                    <td class="px-6 py-4">{user.created_at}</td>
-                    <td class="px-0 py-4">
+                    <td className="px-6 py-4">{user.language}</td>
+                    <td className="px-6 py-4">{user.stdin}</td>
+                    <td className="px-6 py-4">{user.created_at}</td>
+                    <td className="px-0 py-4">
                       {user.code.length > 100
                         ? user.code.slice(0, 100) + "..."
                         : user.code}
--- client/src/Home.js
+++ client/src/Home.js
@@ -27,24 +27,24 @@ function Home() {
   };
 
   return (
-    <div class="text-white bg-black min-h-[100vh] min-w-[100vw]">
-      <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
+    <div className="text-white bg-black min-h-[100vh] min-w-[100vw]">
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <h1 className=" py-[50px] text-3xl">Enter the details:</h1>
-        <div class="mb-5">
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Username
           </label>
           <input
             type="text"
             value={username}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
             required
           />
         </div>
 
-        <div class="mb-5">
+        <div className="mb-5">
           <label
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             htmlFor="coding-language"
@@ -63,7 +63,7 @@ function Home() {
             <option value="Python">Python</option>
           </select>
         </div>
-        <div class="mb-5">
+        <div className="mb-5">
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Standard Input (stdin):
@@ -78,7 +78,7 @@ function Home() {
           </div>
         </div>
         <div className="mb-5">
-          <div class="mb-5">
+          <div className="mb-5">
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Source Code:
             </label>
@@ -93,7 +93,7 @@ function Home() {
         </div>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
